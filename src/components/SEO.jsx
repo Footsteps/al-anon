4760a25246c467ce36908edb14ea5236f43256e8
Angela <angelaschumacher@web.de>
2026-07-20 17:58:20 +0200

    JSON-LD

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,11 +1,22 @@
 import { Helmet } from "react-helmet-async";
 
-export default function SEO({
-  title,
-  description,
-  url,
-  image
-}) {
+export default function SEO({ title, description, url, image }) {
+  const websiteSchema = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: "English Speaking Al-Anon Berlin & European Roundups",
+    url,
+    description,
+  };
+
+  const organizationSchema = {
+    "@context": "https://schema.org",
+    "@type": "NGO",
+    name: "English Speaking Al-Anon Berlin",
+    url,
+    logo: image,
+  };
+
   return (
     <Helmet>
       <title>{title}</title>
@@ -16,15 +27,23 @@ export default function SEO({
 
       {/* Open Graph */}
       <meta property="og:type" content="website" />
-        <meta
+      <meta
         property="og:site_name"
         content="English Speaking Al-Anon Berlin & European Roundups"
       />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content={image}/>
-    
+      <meta property="og:image" content={image || "undefined"} />
+
+      {/* JSON-LD Structured Data */}
+      <script type="application/ld+json">
+        {JSON.stringify(websiteSchema)}
+      </script>
+
+      <script type="application/ld+json">
+        {JSON.stringify(organizationSchema)}
+      </script>
     </Helmet>
   );
 }
